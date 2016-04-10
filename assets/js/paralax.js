31f365b235f6c42214a4833013a999c0d78b75cb
Roro <roromaster@gmail.com>
2016-04-10 20:58:39 +0200

    Updated Parallax

--- assets/js/paralax.js
+++ assets/js/paralax.js
@@ -4,6 +4,31 @@ $(window).scroll(function(){
 
   var wScroll = $(this).scrollTop();
 
+if (wScroll >= $('.intro').offset().top) {
+  console.log("intro zone");
+  $('a[href$=start]').addClass('active-zone');
+  $('a[href$=resume]').removeClass('active-zone');
+}
+if (wScroll >= $('.resume').offset().top){
+  console.log("resume zone");
+  $('a[href$=resume]').addClass('active-zone');
+  $('a[href$=start]').removeClass('active-zone');
+  $('a[href$=testimonials]').removeClass('active-zone');
+}
+if (wScroll >= $('.testimonials-section').offset().top)
+{
+  console.log("testimonials zone");
+  $('a[href$=testimonials]').addClass('active-zone');
+  $('a[href$=resume]').removeClass('active-zone');
+  $('a[href$=contact]').removeClass('active-zone');
+}
+if (wScroll >= $('.contact-page').offset().top)
+{
+  console.log("testimonials zone");
+  $('a[href$=contact]').addClass('active-zone');
+  $('a[href$=testimonials]').removeClass('active-zone');
+}
+
 
 if (wScroll <= pContainerHeight) {
   $('.intro-text').css({
@@ -38,14 +63,14 @@ if (wScroll <= pContainerHeight) {
 
 
   // Floating Elements
-
-  if(wScroll > $('.blog-posts').offset().top - $(window).height()){
-
-    var offset = (Math.min(0, wScroll - $('.blog-posts').offset().top +$(window).height() - 350)).toFixed();
-
-    $('.post-1').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.2) +'px)'});
-
-    $('.post-3').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.2) +'px)'});
-
-  }
+  //
+  // if(wScroll > $('.blog-posts').offset().top - $(window).height()){
+  //
+  //   var offset = (Math.min(0, wScroll - $('.blog-posts').offset().top +$(window).height() - 350)).toFixed();
+  //
+  //   $('.post-1').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.2) +'px)'});
+  //
+  //   $('.post-3').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.2) +'px)'});
+  //
+  // }
 });
